9031f27d9bd1211d7e154ca594fced60445ae092
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2023-10-09 22:43:42 +0300

    feats : chart.js projeye eklendi ve örnek component oluşturuldu.

--- budget-app/src/app/components/chart/chart.component.ts
+++ budget-app/src/app/components/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
 
 @Component({
   selector: 'app-chart',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent {
+  title: string = 'chartDemo';
+
+  ngOnInit() {
+    var myChart = new Chart("myChart", {
+      type: 'pie',
+      data: {
+        labels: [
+          'Expenses',
+          'Incomes',
+          'Loan Payment'
+        ],
+        datasets: [{
+          // label: 'My First Dataset',
+          data: [5400, 14000, 8600],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 1
+        }]
+      }
+    });
+  }
+
+
 
 }
